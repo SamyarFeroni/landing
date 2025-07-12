ef9b5b22f85b703c2e42fd055c6ada97f40e0071
s.feroni <s.feroni@domil.io>
2025-07-13 00:07:33 +0330

    hot fix

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
